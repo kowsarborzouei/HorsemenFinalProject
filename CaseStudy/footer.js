4596210543eb2de1ef2adfe1734170fd9a54cc21
kowsar <k.borzuei@gmail.com>
2021-10-09 21:17:45 +0330

    footer

--- CaseStudy/footer.js
+++ CaseStudy/footer.js
@@ -1,8 +1,8 @@
 function error(){
     var x=document.getElementById(`input`).value;
     if(x==""){
-        $('.error_text').html()="";
-        $('.error_text').append('please try again');
-        $('.red').css('visibility','visible')
+        ('.error_text').html()="";
+        ('.error_text').append('please try again');
+        ('.red').css('visibility','visible')
     }
 }

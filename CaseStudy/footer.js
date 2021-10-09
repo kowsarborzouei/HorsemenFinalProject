e29ced59efa9533ff47155c45cce4854a4b9ca5f
Mahed Yarmohammadi <Mahedyar@gmail.com>
2021-10-09 20:18:30 +0200

    Footer Error Script has been fixed

--- CaseStudy/footer.js
+++ CaseStudy/footer.js
@@ -1,8 +1,15 @@
-function error(){
-    var x=document.getElementById(`input`).value;
-    if(x==""){
-        ('.error_text').html()="";
-        ('.error_text').append('please try again');
-        ('.red').css('visibility','visible')
-    }
+// function error(){
+//     // var x=document.getElementById(`input`).value;
+//     // if(x==""){
+//     //     ('.error_text').html()="";
+//     //     ('.error_text').append('please try again');
+//     //     ('.red').css('visibility','visible')
+//     // }
+//
+// }
+
+
+const error = () => {
+    const val = document. querySelector('input'). value
+    console.log(val)
 }
